4738535c745103a0a88e7b944a19d442c97ef481
anna_b <nelson_anna9@hotmail.com>
2025-09-12 11:13:12 -0500

    Create new recipe form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import RecipeExcerpt from "./components/RecipeExcerpt";
-import RecipeFull from "./components/RecipeFull"
+import RecipeFull from "./components/RecipeFull";
+import NewRecipeForm from "./components/NewRecipeForm";
 import "./App.css";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
   const [selectedRecipe, setSelectedRecipe] = useState(null)
+  const [showNewRecipeForm, setShowNewRecipeForm] = useState(false)
+  const [newRecipe, setNewRecipe] = useState({
+    title: "",
+    ingredients: "",
+    instructions: "",
+    servings: 1, // conservative default
+    description: "",
+    image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" //default
+  })
 
   const handleSelectRecipe = (recipe) => {
     setSelectedRecipe(recipe)
@@ -16,6 +26,20 @@ function App() {
   const handleUnselectRecipe = () => {
     setSelectedRecipe(null)
   }
+
+  const showRecipeForm = () => {
+    setShowNewRecipeForm(true)
+    setSelectedRecipe(null)
+  }
+
+  const hideRecipeForm = () => {
+    setShowNewRecipeForm(false)
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewRecipe({ ...newRecipe, [name]: value });
+}
 
   const fetchAllRecipes = async() => {
     try {
@@ -37,7 +61,8 @@ function App() {
 
   return (
     <div className='recipe-app'>
-      <Header />
+      <Header showRecipeForm={showRecipeForm} />
+      {showNewRecipeForm === true && <NewRecipeForm newRecipe={newRecipe} hideRecipeForm={hideRecipeForm} handleChange={handleChange} />}
       {selectedRecipe === null ? (
         <div className="recipe-list">
           {recipes.map((recipe) => (
---
+++ src/components/NewRecipeForm.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const NewRecipeForm = ({ newRecipe, hideRecipeForm, handleChange }) => {
+    return (
+        <div className='recipe-details'>
+            <div className='recipe-form'>
+                <h2>New Recipe</h2>
+                <button className='cancel-button' onClick={hideRecipeForm}>Cancel</button>
+                <form>
+                    <label htmlFor="title">Title</label>
+                    <input id="title" type='text' name='title' value={newRecipe.title} onChange={(e) => handleChange(e)} required />
+            
+                    <label htmlFor="ingredients">Ingredients</label>
+                    <textarea
+                        id="ingredients"
+                        name='ingredients'
+                        value={newRecipe.ingredients}
+                        onChange={(e) => handleChange(e)}
+                        required
+                        placeholder='Add ingredients separated by commas - i.e. Flour, sugar, almonds'
+                    />
+            
+                    <label htmlFor="instructions">Instructions</label>
+                    <textarea id="instruction" name='instructions' value={newRecipe.instructions} onChange={(e) => handleChange(e)} required />
+            
+                    <label htmlFor="description">Description</label>
+                    <textarea id="description" name='description' value={newRecipe.description} onChange={(e) => handleChange(e)} required />
+            
+                    <label htmlFor="image">Image</label>
+                    <input id="image" type='text' name='image_url' value={newRecipe.image_url} onChange={(e) => handleChange(e)} required />
+            
+                    <label htmlFor="servings">Servings</label>
+                    <input id="servings" type='number' name='servings' value={newRecipe.servings} onChange={(e) => handleChange(e)} required />
+                    <button type='submit'>Save Recipe</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default NewRecipeForm;
